05d4a593940f33924823f527f961931a412e039c
md-liton <mdliton5506@gmail.com>
2024-07-05 05:09:08 +0600

    tostify

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,14 @@
 import React, {  useRef, useState } from 'react'
 import { FaTelegramPlane } from "react-icons/fa";
 import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 
 
 const Contact = () => {
+
+  const navigate = useNavigate()
   const form = useRef();
 
 
@@ -12,6 +17,8 @@ const Contact = () => {
   const [nameErr,setNameErr]=useState('')
   const [email,setEmail]=useState('')
   const [emailErr,setEmailErr]=useState('')
+
+  const [massage,setMassage]=useState('')
 
   
 
@@ -52,6 +59,10 @@ const Contact = () => {
         .then(
           () => {
             console.log('SUCCESS!');
+            toast.success('Successfully Done')
+            setTimeout(()=>{
+              navigate('https://mdliton.vercel.app/')
+            },2000)
           },
           (error) => {
             console.log('FAILED...', error.text);
@@ -86,17 +97,27 @@ const Contact = () => {
             </div>
           </div>
           <div className='w-1/2 border border-2 rounded-lg border-[#37B7C3] p-[40px]'>
-
-
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="dark"
+/>
             <form  ref={form} onSubmit={sendEmail}>
               <div className='flex items-center gap-[50px]' >
                 <div>
-                  <p className='text-[15px] font-semibold   text-[#37B7C3] pl-[10px]'>Name</p>
+                  <p className='text-[15px] font-semibold   text-[#37B7C3] pl-[10px] mb-[5px]'>Name</p>
                   <input onChange={handleName} className='border border-2 rounded-lg border-[#37B7C3] bg-transparent p-[10px] text-[#999999]' placeholder='Name' type="text" name='client_name' />
                   <p className='text-[15px] font-semibold mb-[5px]  text-red-500 pl-[10px] h-[10px]'>{nameErr}</p>
                 </div>
                 <div>
-                  <p className='text-[15px] font-semibold mb-[5px]  text-[#37B7C3] pl-[10px]'>Email</p>
+                  <p className='text-[15px] font-semibold  text-[#37B7C3] pl-[10px] mb-[5px]'>Email</p>
                   <input onChange={handleEmail} className='border border-2 rounded-lg border-[#37B7C3] bg-transparent p-[10px] text-[#999999]' placeholder='Email' type="email" name='client_email' />
                   <p className='text-[15px] font-semibold mb-[5px]  text-red-500 pl-[10px] h-[10px]'>{emailErr}</p>
                 </div>
@@ -112,7 +133,7 @@ const Contact = () => {
                 </div>
               </div>
               <div className='mt-[40px] rounded-lg'>
-                <textarea className='h-[300px] w-full bg-transparent border border-2 rounded-lg border-[#37B7C3] text-[18px] text-white p-[10px]' placeholder='Massage' name='message'></textarea>
+                <textarea  className='h-[300px] w-full bg-transparent border border-2 rounded-lg border-[#37B7C3] text-[18px] text-white p-[10px]' placeholder='Massage' name='message'></textarea>
               </div>
 
               <button type="submit" value="Send" className='text-center text-white font-semibold text-[18px] border border-[#37B7C3]  border-2 rounded-lg py-[8px] px-[25px]  hover:ease-in hover:duration-300  hover:border-[#45f3ff] flex items-center justify-center gap-[10px] cursor-pointer mt-[15px]' >Send <FaTelegramPlane /></button>
