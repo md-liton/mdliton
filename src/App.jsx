543e4feb74044b4938a6fe3c553f9de4c38e1c4e
md-liton <mdliton5506@gmail.com>
2024-07-30 19:42:52 +0600

    screen

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
 
   return (
     <>
-      <div className=" bg-[#081b29] md:py-[20px] xl:py-0" >
+      <div className=" bg-[#081b29] " >
         <RouterProvider router={router} />
       </div>
     </>
